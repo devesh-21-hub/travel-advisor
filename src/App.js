36fc9c7c17e91bd298406c003d7208f0ecc3a2d9
Devesh Kumar <deveshkumar21101@gmail.com>
2022-07-11 14:48:29 +0530

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
+import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData } from "./api";
+
+const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+
+  //Getting user's location for the first time the page loads
+  useEffect(() => {
+    //Using inbuilt location api of browser
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, [coordinates, bounds]);
+  return (
+    <div>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List places={places} />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/api/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    //request
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": "5d3a4c34aemsh4a98a0c8a8d2cb2p12c3e0jsn2cb135ad4c99",
+      },
+    });
+    return data;
+  } catch (error) {}
+};
